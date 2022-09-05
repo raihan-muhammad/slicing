fa74ee943cd2c0f853223cd7ee20408bb4a5723e
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2022-09-05 23:59:07 +0700

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				main: '#1993ab',
+				'neutral/50': '#AEAEAE',
+				'neutral/60': '#8E8E8E',
+				'neutral/80': '#575757',
+				'neutral/70': '#696969',
+				'class-border': '#C9C9C9',
+			},
+		},
+	},
+	plugins: [],
+};
